a217764051d71480991571fad0bd1d22491d6988
Sergio Peña <sergiop79@gmail.com>
2014-11-08 21:01:20 +0100

    Added tests and Gruntfile

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var fortune = require('./lib/fortune');
 var express = require('express');
 var app = express();
@@ -6,21 +8,44 @@ var exphbs = require('express-handlebars');
 app.set('port', process.env.PORT || 3000);
 
 // set up handlebars view engine
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 // static files
 app.use(express.static(__dirname + '/public'));
 
-// new routes
+// middleware for tests
+app.use(function (req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+  next();
+});
+
+// routes
 app.get('/', function (req, res) {
   res.render('home');
 });
 
 app.get('/about', function (req, res) {
-  res.render('about', { fortune: fortune.getFortune() });
+  res.render('about', {
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+  });
 });
 
+app.get('/tours/hood-river', function (req, res) {
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function (req, res) {
+  res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+  res.render('tours/request-group-rate');
+});
 
 // custom 404 page
 app.use(function (req, res) {
@@ -29,7 +54,7 @@ app.use(function (req, res) {
 });
 
 // custom 505 page
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   console.error(err.stack);
   res.status(500);
   res.render('500');
@@ -38,5 +63,5 @@ app.use(function (err, req, res, next) {
 // the server is listening
 app.listen(app.get('port'), function () {
   console.log('Express started on http://localhost:' +
-             app.get('port') + '; press Ctrl + C to terminate.');
+    app.get('port') + '; press Ctrl + C to terminate.');
 });
