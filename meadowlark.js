28a9ecbba558bec250849c45b8ac6059c7f923b1
Sergio Peña <sergiop79@gmail.com>
2014-11-10 13:15:46 +0100

    Added form to upload photos with formidable

--- meadowlark.js
+++ meadowlark.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var formidable = require('formidable');
 
 //config handlebars
 var hbs = exphbs.create({
@@ -96,6 +97,28 @@ app.post('/process', function (req, res) {
   } else {
     res.redirect(303, '/thank-you');
   }
+});
+
+app.get('/contest/vacation-photo', function (req, res) {
+  var now = new Date();
+  res.render('contest/vacation-photo', {
+    year: now.getFullYear(),
+    month: now.getMonth()
+  });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function (req, res) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    if (err) {
+      return res.redirect(303, '/error');
+    }
+    console.log('received fields:');
+    console.log(fields);
+    console.log('received files:');
+    console.log(files);
+    res.redirect(303, '/thank-you');
+  });
 });
 
 // custom 404 page
