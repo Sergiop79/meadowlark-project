de96d0a82990285f7a4c00c61df9673e800b0997
Sergio Peña <sergiop79@gmail.com>
2014-11-13 15:25:54 +0100

    Playing with middelwares

--- meadowlark.js
+++ meadowlark.js
@@ -226,7 +226,7 @@ app.use(function (req, res) {
 });
 
 // custom 505 page
-app.use(function (err, req, res) {
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500);
   res.render('500');
